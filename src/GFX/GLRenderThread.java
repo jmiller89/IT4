ffa11a2b24b2cb249f562561257ee0dfb5fccb0b
jmiller89 <jmiller11689@gmail.com>
2016-07-09 18:14:13 -0400

    Added more tiles and 2 more npc types.

--- src/GFX/GLRenderThread.java
+++ src/GFX/GLRenderThread.java
@@ -117,7 +117,7 @@ public class GLRenderThread extends Canvas implements GameWindowCallback
     private static final String TEXT = "Sprites/IT3Text.png"; //was .gif
     private static final String OVERLAY = "Sprites/overlay.png";
     private LWJGLSprite spritesheet;
-    public static final int NUMSPRITES = 530;
+    public static final int NUMSPRITES = 551;
 
     private LWJGLSprite text;
 
